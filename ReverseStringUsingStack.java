a17765d401e4d8380716a309443e08303cc86bbf
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2023-03-10 19:35:36 +0530

    Add files via upload

---
+++ ReverseStringUsingStack.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseStringUsingStack {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+
+        Stack<Character> stack = new Stack<Character>();
+
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+
+        String reversed = "";
+        while (!stack.empty()) {
+            reversed += stack.pop();
+        }
+
+
+        System.out.println("Reversed string: " + reversed);
+    }
+}
